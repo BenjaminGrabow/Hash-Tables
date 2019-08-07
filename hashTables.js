7171f33083657ded5b741a0b5f596fd73510453c
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-07 19:27:16 +0200

    added notes about hash tables

---
+++ hashTables.js
@@ -0,0 +1,5 @@
+// Hash Tables are used to store data 
+// in a form of a key and associated with a value 
+// key mostly string and the value can be everytime data type
+
+// runtime O(1) (constant runtime)
