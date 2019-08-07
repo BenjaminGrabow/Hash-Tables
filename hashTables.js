036069c073c5fc2a21c6d1757e3736a8c3508278
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-07 19:49:11 +0200

    added comparison between array and linked list

--- hashTables.js
+++ hashTables.js
@@ -10,3 +10,22 @@
 // two types: 
 // singly linked list: each node has reference to the next node 
 // doubly linked list: each node has reference to the next AND previous node
+
+// comparison between array and linked list 
+
+//  Array                      Linked List
+// -fixed size                -dynamic size  
+
+// -inefficient Insert-       -efficient Insertions
+//ions and Deletions            and Deletions
+
+//  -Random access (          - no random access
+//  efficient indexing)
+
+//  May result in much        - no waste of memory
+// memory waste
+
+// Sequential access if       -Sequential access is slow 
+// faster [Reason:            [Reason: Elements not in contiguous
+// Elements in contiguous     locations]
+// memory locations]
